9a7d9fd335b652d99df961c1e2887c4e04f5d50a
Mouadbhz <101869459+Mouadbhz@users.noreply.github.com>
2023-11-06 19:22:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const text = document.getElementById("text");
+console.log(text.value);
+text.onclick = clear ;
+ function clear() {
+  
+    //document.getElementById("text").value = " ";
+   console.log(document.getElementById("text").value);
+}
+console.log("hi");
